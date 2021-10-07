a3b3d74b50807df121a95c75c54fd085c8de698f
Edon@k <edouardnakasila69@gmail.com>
2021-10-07 04:04:45 +0100

    creation et connexion database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express("app");
 const path = require("path");
+const mysql = require('mysql');
 const PORT  = 9996;
 
 //init libraries
@@ -14,6 +15,21 @@ app.set("view engine", require("ejs").renderFile);
 //init routes
 var baseRoutes = require("./routes/baseRoutes");
 app.use("/", baseRoutes);
+
+//creation et connexion database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+    db.query("CREATE DATABASE blackdb", function (err, result) {
+      if (err) throw err;
+      console.log("Base de données créée !");
+    });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
