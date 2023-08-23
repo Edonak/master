b7cf83878bbf944a19fbcb287ffe94320e16bac5
Edonak <edouardnakasila69@gmail.com>
2023-08-23 10:34:05 -0700

    to update what will be committed

--- app.js
+++ app.js
@@ -16,20 +16,6 @@ app.set("view engine", require("ejs").renderFile);
 var baseRoutes = require("./routes/baseRoutes");
 app.use("/", baseRoutes);
 
-//creation et connexion database
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-  });
-  db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connecté à la base de données MySQL!");
-    db.query("CREATE DATABASE blackdb", function (err, result) {
-      if (err) throw err;
-      console.log("Base de données créée !");
-    });
-  });
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
